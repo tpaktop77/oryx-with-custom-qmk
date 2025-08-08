795b787f72fe27295f6a36ad9b5598b8854b353e
Vitaly Belousov <vitaly.belousov@gmail.com>
2025-08-08 12:15:10 -0700

    Update switch_cases.c

--- 6JP4n/modules/switch_cases.c
+++ 6JP4n/modules/switch_cases.c
@@ -95,7 +95,8 @@
 #if defined(OS_DETECTION_ENABLE)
                 os_variant_t host_os = detected_host_os();
                 if (host_os == OS_MACOS || host_os == OS_IOS) {
-                    tap_code16(MAC_SIRI);
+                    //tap_code16(MAC_SIRI);
+                    HCS(0xCF);
                 } else  {
                     SEND_STRING(SS_LGUI("h"));
                 }
